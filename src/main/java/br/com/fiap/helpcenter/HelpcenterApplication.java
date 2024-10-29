c9b861669b8b200755da8e1ddb8def2e60e350ed
yaslopesyweb <slahyasmin@gmail.com>
2024-10-29 10:33:15 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/helpcenter/HelpcenterApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.helpcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpcenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelpcenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/helpcenter/HelpcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.helpcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpcenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
